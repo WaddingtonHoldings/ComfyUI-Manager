2560ee5bfc313a5431cdf951f16e769c4f0812dd
Milad M <mesbahmilad@gmail.com>
2025-09-14 13:37:37 -0700

    Fixing bug

--- glob/manager_core.py
+++ glob/manager_core.py
@@ -731,18 +731,14 @@ class UnifiedManager:
 
         json_obj = await get_data_by_mode(mode, 'custom-node-list.json', channel_url=channel_url)
         for x in json_obj['custom_nodes']:
-            try:
-                for y in x['files']:
-                    if 'github.com' in y and not (y.endswith('.py') or y.endswith('.js')):
-                        repo_name = y.split('/')[-1]
-                        res[repo_name] = (x, False)
+            for y in x['files']:
+                if 'github.com' in y and not (y.endswith('.py') or y.endswith('.js')):
+                    repo_name = y.split('/')[-1]
+                    res[repo_name] = (x, False)
 
-                if 'id' in x:
-                    if x['id'] not in res:
-                        res[x['id']] = (x, True)
-            except:
-                logging.error(f"[ComfyUI-Manager] broken item:{x}")
-
+            if 'id' in x:
+                if x['id'] not in res:
+                    res[x['id']] = (x, True)
 
         return res
 
